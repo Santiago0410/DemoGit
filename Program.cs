69f4f371057f33c291b34a180d727c15710a3f7f
SantiagoCamacho <enano_castro@hotmail.com>
2018-08-31 11:21:23 -0600

    Version 1.1
    Se agrego Linea del profe Saludo

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace DemoGit
 {
     class Program
     {
+        public string Saludo { get; set; }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hola");
